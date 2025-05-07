ef3da182683103ba0c5b0c2305617ac0d565052b
VTSRepo <vibhavatechnologyservices@gmail.com>
2025-05-08 08:24:03 +1000

    status property update in indent report

--- src/utilities/service.js
+++ src/utilities/service.js
@@ -269,7 +269,7 @@ export const getProductReport = async (param) => {
 export const getIndentReport = async (param) => {
   try {
     const org_id = JSON.parse(localStorage.getItem("user"))?.org_id;
-    let url = `${API_URL}/indent-report/${org_id}?from_date='${param.from_date}'&&to_date='${param.to_date}'&&stutus=${param.status}`;
+    let url = `${API_URL}/indent-report/${org_id}?from_date='${param.from_date}'&&to_date='${param.to_date}'&&status=${param.status}`;
     const response = await axios.get(url);
     return response.data; // Return only the data from the response
   } catch (error) {
